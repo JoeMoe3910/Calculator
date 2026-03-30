582ddd8a4cdd93821ed52816252e63ca36b6ca9b
JoeMoe3910 <130257577+JoeMoe3910@users.noreply.github.com>
2026-03-30 18:07:36 +0300

    2.0.0
    Обновлены ссылки (теперь правильные)
    добавлена иконка.
    Теперь есть деинсталятор.
    Добавленно автоматическое обновление.

--- build_exe.bat
+++ build_exe.bat
@@ -11,12 +11,13 @@ echo.
 call C:\Users\Inna\venv\Scripts\activate.bat
 
 echo [1/3] Установка/обновление PyInstaller...
-pip install pyinstaller --quiet
+pip install pyinstaller flet flet-cli flet-desktop --quiet
 
 echo [2/3] Сборка EXE с иконкой...
-pyinstaller --onefile --noconsole --name "Калькулятор" --icon "icon.ico" main.py
+pyinstaller --onefile --noconsole --name "Calculator" --icon "icon.ico" --hidden-import "flet_desktop" --hidden-import "flet_core" main.py
 
 echo [3/3] Создание ZIP архива...
+ren dist\Calculator.exe Калькулятор.exe
 powershell -Command "Compress-Archive -Path 'dist\Калькулятор.exe' -DestinationPath 'dist\Калькулятор.zip' -Force"
 
 echo.
